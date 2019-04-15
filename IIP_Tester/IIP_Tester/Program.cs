9eb8e2a6f8d086ade33a7b22ba778aa72866c010
Alexander <rozov.mining@yandex.com>
2019-04-15 12:09:34 +0300

    15.04.2019 COM Port scanning

--- IIP_Tester/IIP_Tester/Program.cs
+++ IIP_Tester/IIP_Tester/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Net.Sockets;
+using System.Management;
+using Microsoft.Win32;
 
-namespace IIP_Tester
+namespace SupplyTester_Interface
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Clear();
+
+            IIP_TESTER.Connect();
+
+            Console.ReadLine();
         }
+
     }
 }
+
